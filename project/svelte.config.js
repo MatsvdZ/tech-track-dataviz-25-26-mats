837ab3ef97680f0db2c62ac2f64f410b6d341ade
MatsVDZ <matshaarlem2005@gmail.com>
2025-11-23 23:07:59 +0100

    vercel push

--- project/svelte.config.js
+++ project/svelte.config.js
@@ -1,20 +1,8 @@
-import adapter from '@sveltejs/adapter-netlify';
+import vercel from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// default options are shown
-		adapter: adapter({
-			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions
-			edge: false,
-
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app.
-			// if `edge` is true, this option cannot be used
-			split: false
-		})
-	}
+export default {
+  kit: {
+    adapter: vercel(),
+    // andere instellingen blijven hetzelfde
+  }
 };
-
-export default config;
